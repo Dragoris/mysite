724c3efef7aac87a6174b25e31b1e1b0f7d8ec3a
matt <mtrower42@yahoo.com>
2016-05-19 23:29:46 -0700

    added an extra parallax effect by using margin-top and scroll distance. Can be added elsewhere as needed

--- static/moly.js
+++ static/moly.js
@@ -3,15 +3,27 @@ $(function() {
     var header = $(".vertical-header");
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
-        if (scroll >= 500) {
+        if (scroll >= 650) {
             header.removeClass("vertical-header").addClass("horizontal-header");
         } else {
             header.removeClass("horizontal-header").addClass("vertical-header");
         }
-        if (scroll >= 700){
+        if (scroll >= 750){
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
     });
+});
+
+
+$(document).scroll(function() {
+    var position = $(document).scrollTop();
+    if (position <= 650){
+        var margin = parseInt(position * -0.3) + 'px';
+        $('.vertical-header').css('margin-top', margin);
+    } else{
+        $('.vertical-header').css('margin-top', 0);
+    }
+
 });
