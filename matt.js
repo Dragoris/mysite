d733eb2da648e4f6591a47f52227deb6b82acf94
matt <mtrower42@yahoo.com>
2016-06-05 23:25:33 -0700

    set up social icons, lines through section headings, updated media query

--- matt.js
+++ matt.js
@@ -1,4 +1,19 @@
-window.addEventListener('scroll', function () {
+// media query event handler
+if (matchMedia) {
+	console.log('1')
+  var mq = window.matchMedia("(min-width: 600px)");
+  mq.addListener(WidthChange);
+  WidthChange(mq);
+}
+
+// media query change
+function WidthChange(mq) {
+  if (mq.matches) {
+    // window width is at least 600px
+    window.addEventListener('scroll', function () {
     var scrolled = window.scrollY;
-    document.querySelector('.banner h1').style.marginTop =  (35+scrolled*0.75)+ "px";
-});
+    	document.querySelector('.banner h1').style.marginTop =  (35+scrolled*0.75)+ "px";
+	});
+  } 
+
+}
