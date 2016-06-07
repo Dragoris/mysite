f1896c1cb579dadaf6e5c8628eaf7f0307c2ccb7
matt <mtrower42@yahoo.com>
2016-06-06 23:27:06 -0700

    added a HTML form w/ Formspree

--- matt.js
+++ matt.js
@@ -1,7 +1,7 @@
 // media query event handler
 if (matchMedia) {
 	console.log('1')
-  var mq = window.matchMedia("(min-width: 600px)");
+  var mq = window.matchMedia("(min-width: 625px)");
   mq.addListener(WidthChange);
   WidthChange(mq);
 }
@@ -12,7 +12,7 @@ function WidthChange(mq) {
     // window width is at least 600px
     window.addEventListener('scroll', function () {
     var scrolled = window.scrollY;
-    	document.querySelector('.banner h1').style.marginTop =  (35+scrolled*0.75)+ "px";
+    	document.querySelector('.banner h1').style.marginTop =  (35+scrolled*0.70)+ "px";
 	});
   } 
 
