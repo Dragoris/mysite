3e1bf8a8f13f4020510324c201ee8cbe2629f79d
matt <mtrower42@yahoo.com>
2016-06-17 16:05:49 -0700

    parallax only works on >1100px screens, resets on <1100px. Removed box-shadow from contact. Added comments

--- matt.js
+++ matt.js
@@ -1,24 +1,18 @@
+$(function () {
+    $(window).resize(function () { 
+        if ($(window).width() > 1000) { //If >1000px show parallax effect
+          window.addEventListener('scroll', function () {
+          var scrolled = window.scrollY;
+          document.querySelector('.banner h1').style.marginTop =  (35+scrolled*0.7)+ "px";
+          })
+        }else{ //else set margin to 0 (ie. no parallax)
+          window.addEventListener('scroll', function () {
+          document.querySelector('.banner h1').style.marginTop = 0+ "px";
+          })
+        }
+    }).resize(); //call resize function 
+});
 
-
-
-// media query event handler
-if (matchMedia) {
-  var mq = window.matchMedia("only screen and (min-width: 625px)");
-  mq.addListener(WidthChange);
-  WidthChange(mq);
-}
-
-// media query change
-function WidthChange(mq) {
-  if (mq.matches) {
-    // window width is at least 990px
-    window.addEventListener('scroll', function () {
-    var scrolled = window.scrollY;
-    	document.querySelector('.banner h1').style.marginTop =  (35+scrolled*0.7)+ "px";
-	});
-  } 
-
-}
 //scroll to location of an id matching the link hash
 $('a[href^="#"]').click(function(e) {
   $('html,body').animate({ scrollTop: $(this.hash).offset().top}, 1000); 
